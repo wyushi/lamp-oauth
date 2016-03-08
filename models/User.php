8d7695b41e45cd8053002d4244ae4ca7cccffdca
Yushi Wang <jasper.ys.wang@gmail.com>
2016-03-07 17:37:55 -0700

    add user api get, post

--- models/User.php
+++ models/User.php
@@ -4,21 +4,73 @@ use lib\Core;
 use PDO;
 
 class User {
-	protected $core;
+    protected $core;
 
-	function __construct() {
-		$this->core = Core::getInstance();
-	}
+    function __construct() {
+        $this->core = Core::getInstance();
+    }
 
-	public function getUsers() {
-		$r = array();
-		$sql = "SELECT * FROM account";
-		$stmt = $this->core->dbh->prepare($sql);
-		if ($stmt->execute()) {
-			$r = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		} else {
-			$r = 0;
-		}
-		return $r;
-	}
+    public function getUsers() {
+        $r = array();
+        $sql = "SELECT * FROM account";
+        $stmt = $this->core->dbh->prepare($sql);
+        if ($stmt->execute()) {
+            $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $r = 0;
+        }
+        return $r;
+    }
+
+    public function getUserById($id) {
+        $r = array();       
+        
+        $sql = "SELECT nombre * evnt_usuario WHERE id=$id";
+        $stmt = $this->core->dbh->prepare($sql);
+        //$stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $r = $stmt->fetchAll(PDO::FETCH_ASSOC);         
+        } else {
+            $r = 0;
+        }       
+        return $r;
+    }
+
+    public function getUserByLogin($email, $pass) {
+        $r = array();       
+        
+        $sql = "SELECT * FROM user WHERE email=:email AND password=:pass";      
+        $stmt = $this->core->dbh->prepare($sql);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':pass', $pass, PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            $r = $stmt->fetchAll(PDO::FETCH_ASSOC);         
+        } else {
+            $r = 0;
+        }       
+        return $r;
+    }
+
+    public function insertUser($data) {
+        try {
+            $sql = "INSERT INTO account (username, email) VALUES (:username, :email)";
+            $stmt = $this->core->dbh->prepare($sql);
+            if ($stmt->execute($data)) {
+                $data['id'] = $this->core->dbh->lastInsertId();
+                return $data;
+            } else {
+                return '0';
+            }
+        } catch(PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function updateUser($data) {
+        
+    }
+
+    public function deleteUser($id) {
+        
+    }
 }
--- routers/user.php
+++ routers/user.php
@@ -2,8 +2,17 @@
 use models\User;
 
 $app->get('/users', function ($request, $response, $args) {
-	$model = new User();
-	$users = $model->getUsers();
+    $model = new User();
+    $users = $model->getUsers();
     $response->getBody()->write(json_encode($users));
+    return $response;
+});
+
+$app->post('/users', function ($request, $response, $args) {
+    $parsed = $request->getParsedBody();
+    $model = new User();
+    $user = $model->insertUser($parsed);
+    $response = $response->withHeader('Content-type', 'application/json');
+    $response->getBody()->write(json_encode($user));
     return $response;
 });
